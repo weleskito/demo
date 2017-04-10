d8728a4f4ddb1342971f6b47be46cd1681e63fda
Hammat Wele <Hammat Wele@PC-user01>
2017-04-10 15:57:54 +0000

    qwertyqwerty

--- index.php
+++ index.php
@@ -14,6 +14,11 @@ and open the template in the editor.
         // put your code here
         echo "ceci est un test qwerty";
         echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
         ?>
     </body>
 </html>

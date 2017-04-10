bf6bf417ca7e10432ef8d3d5f415575209be4518
Hammat Wele <Hammat Wele@PC-user01>
2017-04-10 17:07:06 +0000

    zerty

--- index.php
+++ index.php
@@ -13,6 +13,11 @@ and open the template in the editor.
         <?php
         // put your code here
         echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
+        echo "ceci est un test qwerty";
         echo "ceci est un test aqwxsz";
         
         ?>

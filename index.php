8a704f235ead26c293768206651db9b6f07cf7ea
Hammat Wele <Hammat Wele@PC-user01>
2017-04-10 12:01:54 +0000

    qwerty

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "ceci est un test";
+        echo "ceci est un test qwerty";
         ?>
     </body>
 </html>

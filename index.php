a2c24c884359e5148bde0dd9cd000dd1567c07f3
Hammat Wele <Hammat Wele@PC-user01>
2017-04-11 14:21:20 +0000

    rrr

--- index.php
+++ index.php
@@ -23,7 +23,11 @@ and open the template in the editor.
         echo "ceci est un test aqwxsz";
         echo "ceci est un test aqwxsz";
         echo "ceci est un test aqwxsz";
-        echo "ceci est un test Hammat";
+        echo "ceci est un test test";
+        echo "ceci est un test test";
+        echo "ceci est un test test";
+        echo "ceci est un test test";
+        echo "ceci est un test test";
         echo "ceci est un test Hammat";
         echo "ceci est un test Hammat";
         echo "ceci est un test Hammat";
